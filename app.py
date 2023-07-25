99e7daf106cff15333bfcf157287d97299d498c8
Jacob Beran <37278219+jakeberan@users.noreply.github.com>
2023-07-24 22:11:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(5)))
+url_link = "https://www.fantasypros.com/nfl/adp/overall.php"
+fpros = pd.read_html(url_link)[0]
 
-st.table(df)
+st.table(fpros)
